595daef89d9feb82d07553cb5ce212cd1b6b0165
hack_이승철 <sclee@afreecatv.com>
2023-05-01 04:40:12 +0900

    상세페이지 insets 세팅

--- DoubleCheckCall/View/PhoneInfoView.swift
+++ DoubleCheckCall/View/PhoneInfoView.swift
@@ -13,18 +13,97 @@ struct PhoneInfoView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 20) {
-                    //GroupBox(
+            ScrollView(.vertical, showsIndicators: true) {
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
+                        GroupBox {
+                            Button(action: {
+                                phoneCall(number: personInfo.mobile)
+                            }) {
+                              Image(systemName: "message")
+                                    .resizable()
+                            }
+                        }
+                        .padding(10)
+                        .border(Color.red)
+                        GroupBox {
+                            Button(action: {
+                                phoneCall(number: personInfo.mobile)
+                            }) {
+                              Image(systemName: "phone")
+                                    .resizable()
+                            }
+                        }
+                        .padding(10)
+                        .border(Color.red)
+                        GroupBox {
+                            Button(action: {
+                                phoneCall(number: personInfo.mobile)
+                            }) {
+                              Image(systemName: "video")
+                                    .resizable()
+                            }
+                        }
+                        .padding(10)
+                        .border(Color.red)
+                        GroupBox {
+                            Button(action: {
+                                phoneCall(number: personInfo.mobile)
+                            }) {
+                              Image(systemName: "mail")
+                                    .resizable()
+                            }
+                        }
+                        .padding(10)
+                        .border(Color.red)
+                    }
+                    
+                    GroupBox(label: Text("메모")) {
+                        Divider().padding(.vertical, 4)
                         Button(action: {
                             phoneCall(number: personInfo.mobile)
                         }) {
-                          Image(systemName: "phone")
+                            Text("메세지보내기")
+                                .padding(.leading, 0)
                         }
-                    //)
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                    
+                    Spacer(minLength: 10)
+                    
+                    GroupBox {
+                        HStack {
+                            Button(action: {
+                                phoneCall(number: personInfo.mobile)
+                            }) {
+                                Text("연락처 공유")
+                                    .padding(.leading, 0)
+                            }
+                            Spacer()
+                        }
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                    
+                    Spacer(minLength: 10)
+                    
+                    GroupBox {
+                        HStack {
+                            Button(action: {
+                                phoneCall(number: personInfo.mobile)
+                            }) {
+                                Text("이 발신자 차단")
+                                    .padding(.leading, 0)
+                            }
+                            Spacer()
+                        }
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
                 }
+                
+                .navigationViewStyle(StackNavigationViewStyle())
+                .navigationBarTitle(Text(personInfo.name ?? "NONAME"), displayMode: .large)
             }
-        }.padding()
+        }
     }
     
     func phoneCall(number: String?) {
